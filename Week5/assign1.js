985c09d09d35f96bccfca58283b1f44decb80228
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-07 18:47:42 +0530

    Adding remaing files

---
+++ Week5/assign1.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const app = express();
+
+
+function middleware(req,res,next){
+    console.log("Method is : "+req.method);
+    console.log("URL is : "+req.url);
+    console.log("HostName is : "+req.hostname);
+    console.log(new Date());
+    next()
+}
+
+app.use(middleware);
+app.get("/sum", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
