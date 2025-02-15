1c815ef0c02e07ed5a10343b63a400b649b588d5
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-02-15 17:54:32 +0530

    Minor Changes

--- Week13/tailwind/src/App.jsx
+++ Week13/tailwind/src/App.jsx
@@ -4,7 +4,7 @@ function App(){
   // bg-red-500 - unprefixed utility   unprefixed is default  implies mobile-> tablet > desktop 
   //  sm:bg-blue-500 - prefixed  -utility    prefixed utility take effect on specific break point and above      
   return <div className="grid grid-cols-12 ">
-       <div className="col-span-12  sm:col-span-5 bg-red-300">Hey1</div>
+       <div className="col-span-12  sm:col-span-5 bg-[#FF0000]">Hey1</div>
        <div className="col-span-12  sm:col-span-5 bg-green-300">Hey2</div>
        <div className="col-span-12  sm:col-span-2 bg-blue-300">Hey3</div>
 
