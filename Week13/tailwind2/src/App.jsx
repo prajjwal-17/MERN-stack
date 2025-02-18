55cc4bf86e79cdbde11af2808aab0d0f42e51122
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-02-18 23:49:51 +0530

    Completed structure

--- Week13/tailwind2/src/App.jsx
+++ Week13/tailwind2/src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SidebarToggle } from "./components/SidebarToggle";
+
+
+const useMediaQuery=(query)=>{
+  const [matches,setMatches]=useState(false);
+
+  useEffect(()=>{
+    const media=window.matchMedia(query);
+    if(media.matches!==matches){
+      setMatches(media.matches)
+    }
+    const listener=()=>setMatches(media.matches);
+    media.addListener(listener);
+    return ()=>media.removeListener(listener);
+  },[matches,query]);
+  return matches;
+}
+
+
 
 function App(){
 
   const [sidebarOpen,setSidebarOpen]=useState(true);
+  const isDesktop=useMediaQuery("(min-width:768px)");
+
+  useEffect(()=>{
+    if(isDesktop==false) setSidebarOpen(false);
+    else setSidebarOpen(true)
+  },[isDesktop])
   return(
     <div className="flex">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -22,8 +46,8 @@ if(!sidebarOpen){
   </div>
 }
 
- return <div className="w-96 h-screen bg-red-100">
-  <div className="transition-all cursor-pointer hover:bg-white hover:p-9 " onClick={()=>{setSidebarOpen(!sidebarOpen)}}>
+ return <div className=" w-96 h-screen bg-red-100 fixed top-0 left-0 md:relative">
+  <div className="cursor-pointer hover:bg-white hover:p-9 " onClick={()=>{setSidebarOpen(!sidebarOpen)}}>
     <SidebarToggle/>
   </div>
 </div>
@@ -32,15 +56,15 @@ if(!sidebarOpen){
 function MainContent(){
 return (
   <div className="w-full"> 
-    <div className="h-72 bg-black"></div>
-    <div className="grid grid-cols-11 gap-8 p-8">
-      <div className="h-96 rounded-2xl shadow-lg bg-red-300 col-span-2 -translate-y-24">
+    <div className="h-72 bg-black hidden md:block"></div>
+    <div className="transition-all duration-700 grid grid-cols-11 gap-8 p-8">
+      <div className="h-96 rounded-2xl shadow-lg bg-red-300 md:col-span-2 col-span-11 -translate-y-24 hidden md:block">
         {/* Add content for this section */}
       </div>
-      <div className="h-96 rounded-2xl shadow-lg bg-green-300 col-span-6">
+      <div className="h-96 rounded-2xl shadow-lg bg-green-300 md:col-span-6 col-span-11">
         {/* Add content for this section */}
       </div>
-      <div className="h-96 rounded-2xl shadow-lg bg-yellow-300 col-span-3">
+      <div className="h-96 rounded-2xl shadow-lg bg-yellow-300 md:col-span-3 col-span-11">
         {/* Add content for this section */}
       </div>
     </div>
