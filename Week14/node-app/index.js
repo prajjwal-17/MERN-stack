7a887035c817ce8052870f91fee4894254c4229a
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-03-04 16:12:32 +0530

    Basic Typescript

---
+++ Week14/node-app/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// let x=1; //type inferencing--understanding it is a number
+// console.log(x);
+let x = 1; //telling the datatype
+console.log(x);
---
+++ Week14/node-app/index.ts
@@ -0,0 +1,6 @@
+// let x=1; //type inferencing--understanding it is a number
+// console.log(x);
+
+
+let x: number =1;//telling the datatype
+console.log(x);
