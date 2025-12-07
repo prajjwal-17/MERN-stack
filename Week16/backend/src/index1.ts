4014af4acad740aaa5d05337849298cfab924cab
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-07 18:13:03 +0530

    Client to server message

--- Week16/backend/src/index1.ts
+++ Week16/backend/src/index1.ts
@@ -9,11 +9,5 @@ wss.on("connection" , function(socket){
     setInterval(()=>{
         socket.send("Current price of Solana is " + Math.random()*100);
     },500)
-
-    socket.on("message",(e)=>{
-        console.log(e.toString())
-    })
-// for a message from postman we send we get <Buffer 77 68 61 74 20 69 73 20 73 6f 6c 61 6e 61 20 70 72 69 63 65> on logs
-// to get a proper message we do to string
     
 })
