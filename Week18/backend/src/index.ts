06956965723ead112c1eb092b90f3ef8906204a3
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-20 01:17:39 +0530

    Added different prisma style queries

--- Week18/backend/src/index.ts
+++ Week18/backend/src/index.ts
@@ -5,14 +5,55 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient(); 
 
+
 async function createUser(){ 
     await client.user.create({ 
         data : { 
-            username: "Prajjwal", 
+            username: "Tester2", 
             password : "1234", 
-            age : 21, 
+            age : 10, 
             city : "Delhi" 
         } 
     }) 
 } 
-createUser();
+
+
+
+
+async function deleteUser(){
+    await client.user.delete({
+        where : {
+            id : 1
+        }
+    })
+}
+
+
+
+async function updateUser() {
+  await client.user.update({
+    where: {
+      id: 2
+    },
+    data: {
+      username: "TesterUpdated"
+    }
+  });
+}
+
+async function findUser() {
+  const user  = await client.user.findFirst({
+    where: {
+      id: 2
+    }
+
+  });
+  console.log(user?.username); // question mark because user can be null here
+}
+
+
+//Run them only 1 at a time
+// createUser(); // run with diff values
+// deleteUser();
+// updateUser();
+findUser();
