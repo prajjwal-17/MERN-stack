8b68ffe757174dccdd94b9fae624d032fb211814
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-20 01:01:11 +0530

    Made prisma to v6 to fix issue along with readme for setup

--- Week18/backend/src/index.ts
+++ Week18/backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Testing");
+import { PrismaClient } from "@prisma/client";
+
+
+
+
+const client = new PrismaClient(); 
+
+async function createUser(){ 
+    await client.user.create({ 
+        data : { 
+            username: "Prajjwal", 
+            password : "1234", 
+            age : 21, 
+            city : "Delhi" 
+        } 
+    }) 
+} 
+createUser();
