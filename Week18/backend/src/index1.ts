2c4796a0469179ac26726631ffc6f849822dae06
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-20 01:50:14 +0530

    added server to the script

---
+++ Week18/backend/src/index1.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from "@prisma/client";
+
+
+
+
+const client = new PrismaClient(); // find a way to put logs 
+
+
+async function createUser(){ 
+    await client.user.create({ 
+        data : { 
+            username: "Tester2", 
+            password : "1234", 
+            age : 10, 
+            city : "Delhi" 
+        } 
+    }) 
+} 
+
+
+
+
+async function deleteUser(){
+    await client.user.delete({
+        where : {
+            id : 1
+        }
+    })
+}
+
+
+
+async function updateUser() {
+  await client.user.update({
+    where: {
+      id: 2
+    },
+    data: {
+      username: "TesterUpdated"
+    }
+  });
+}
+
+async function findUser() {
+  const user  = await client.user.findFirst({
+    where: {
+      id: 2
+    }
+
+  });
+  console.log(user?.username); // question mark because user can be null here
+}
+
+async function findUser2() {
+  const user  = await client.user.findFirst({
+    where: {
+      id: 2
+    },
+    include : {
+        todos : true  //this will include todos
+    }
+
+  });
+  console.log(user?.username); // question mark because user can be null here
+}
+
+//Run them only 1 at a time
+// createUser(); // run with diff values
+// deleteUser();
+// updateUser();
+// findUser();
+// findUser2();
