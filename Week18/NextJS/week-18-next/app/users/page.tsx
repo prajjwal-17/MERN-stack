1bc1425d0e30db8e950ef9f47aaca50fe84f8e2e
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-21 20:51:00 +0530

    Setting up nextJS

---
+++ Week18/NextJS/week-18-next/app/users/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hey mate this shows page based routing as this file is in users folder inside app
+    </div>
+  );
+}
