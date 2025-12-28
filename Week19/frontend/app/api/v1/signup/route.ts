885224152c2934276922788526af92df8d150100
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-29 00:47:04 +0530

    Basic interaction with database

--- Week19/frontend/app/api/v1/signup/route.ts
+++ Week19/frontend/app/api/v1/signup/route.ts
@@ -1,9 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
 
 export async function POST(req : NextRequest){
 
     const data= await req.json();
     console.log(data);
+
+    await prismaClient.user.create({
+        data : {
+            username : data.username ,
+            name : data.name,
+            password : data.password,
+        }
+    })
 
     return NextResponse.json({
         message : "You have been Signed Up"
